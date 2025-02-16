f7fb364bb3bf8bc10639fc81954bb1da26727dfd
Gokul M R <gokulmr331@gmail.com>
2025-02-16 22:57:47 +0530

    basic page routes setup finished

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage"
+import SignUpPage from "./pages/SignUpPage"
+import LoginPage from "./pages/LoginPage"
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   )
 }
