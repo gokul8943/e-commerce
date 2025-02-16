d7f6789203a0f69d14c1c17939479131a5d72e34
Gokul M R <gokulmr331@gmail.com>
2025-02-16 23:39:05 +0530

    changes

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className='container mx-auto px-4 py-3'>
          <Link to='/' className='tetx-2xl font-bold text-emerald-400 items-center space-x-2 flex'>E-commerce </Link>
          <nav className='flex flex-wrap items-center gap-4'>
-            <Link to='/' className='' ></Link>
+            <Link to='/' className='' >Home</Link>
 
          </nav>
       </div>
