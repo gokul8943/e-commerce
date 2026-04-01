f3415a75829975e824cb443bd8e99cba458015ea
Gokul M R <gokulmr331@gmail.com>
2026-04-01 08:57:07 +0530

    feat: order service created

---
+++ backend/services/order-service/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+const port = process.env.PORT || 5002;
+
+app.listen(port, () => {
+    console.log(`order Service listening on port ${port}`);
+});
